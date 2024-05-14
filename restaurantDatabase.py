fd79dd7bb3ffccc92d30c8ea32380071c49e867c
cellus4853 <134342107+cellus4853@users.noreply.github.com>
2024-05-14 03:42:05 -0400

    Add files via upload

---
+++ restaurantDatabase.py
@@ -0,0 +1,206 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import mysql.connector
+
+class RestaurantDatabase:
+    def __init__(self):
+        self.host = "8000"
+        self.user = "root"
+        self.password = "m6001278#$ER34er"
+        self.database = "restaurant_reservations"
+
+        self.conn = mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.database
+        )
+        self.cursor = self.conn.cursor()
+        
+SyntaxError: multiple statements found while compiling a single statement
+    def close_connection(self):
+        self.cursor.close()
+        self.conn.close()
+        
+SyntaxError: unexpected indent
+create database restaurant_reservations;
+
+create table Customers (
+	customerID int not null unique auto_increment primary key,
+    customerName varchar(50) not null,
+    contactInfo varchar(150),
+    storeEmail varchar(50)
+);
+
+create table Reservations (
+	reservationID int not null unique auto_increment primary key,
+    customerID int not null,
+    reservationTime datetime not null,
+    numberOfGuests int not null,
+    specialRequests varchar(150),
+    foreign key(customerID) references Customers(customerID)
+);
+
+create table DiningPreferences (	
+	preferenceID int not null unique auto_increment primary key,
+    customerID int not null,
+    favoriteTable varchar(50),
+    dietaryRestrictions varchar(150),
+    foreign key(customerID) references Customers(customerID)
+);
+
+DELIMITER //
+CREATE PROCEDURE findReservations(IN customer_ID INT)
+BEGIN
+    SELECT * FROM Reservations WHERE customerID = customer_ID;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE addSpecialRequest(IN reservation_ID INT, IN requests_cell VARCHAR(150))
+BEGIN
+    UPDATE Reservations SET specialRequests = requests_cell WHERE reservationID = reservation_ID;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE addReservation(
+    IN customerName_cell VARCHAR(50),
+    IN contactInfo_cell VARCHAR(150),
+    IN reservationTime_cell DATETIME,
+    IN numberOfGuests_cell INT,
+    IN specialRequests_cell VARCHAR(150)
+)
+BEGIN
+    DECLARE customerID_milt INT;
+    
+    SELECT customerID INTO customerID_milt FROM Customers WHERE customerName = customerName_cell AND contactInfo = contactInfo_cell;
+    
+    IF customerID_milt IS NULL THEN
+        INSERT INTO Customers (customerName, contactInfo) VALUES (customerName_cell, contactInfo_cell);
+        SET customerID_milt = LAST_INSERT_ID();
+    END IF;
+		INSERT INTO Reservations (customerID, reservationTime, numberOfGuests, specialRequests)
+		VALUES (customerID_milt, reservationTime_cell, numberOfGuests_cell, specialRequests_cell);
+    
+END //
+DELIMITER ;
+
+INSERT INTO Customers (customerName, contactInfo) VALUES
+("Julynn Pelius", "jilton@cmail.com"),
+("John Pelius", "johnboy@cmail.com"),
+("Jessiana Pelius", "Jessie@cmail.com");
+
+INSERT INTO Reservations (customerID, reservationTime, numberOfGuests, specialRequests) VALUES
+(0001, "2024-05-11 14:00:00", 6, "Window seat preferred"),
+(0002, "2024-05-12 15:00:00", 12, NULL),
+(0003, "2024-05-13 16:00:00", 9, "Meat options requested");
+
+INSERT INTO DiningPreferences (customerID, favoriteTable, dietaryRestrictions) VALUES
+(0001, "Table 2", "None"),
+(0002, "Table 1", "Gluten-free"),
+(0003, "Table 3", "Vegan");
+SyntaxError: invalid syntax
+    def close_connection(self):
+        self.cursor.close()
+        self.conn.close()
+        
+SyntaxError: unexpected indent
+import mysql.connector
+
+class RestaurantDatabase:
+    def __init__(self):
+        self.host = "8000"
+        self.user = "root"
+        self.password = "m6001278#$ER34er"
+        self.database = "restaurant_reservations"
+
+        self.conn = mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.database
+        )
+        self.cursor = self.conn.cursor()
+        
+create database restaurant_reservations;
+
+create table Customers (
+	customerID int not null unique auto_increment primary key,
+    customerName varchar(50) not null,
+    contactInfo varchar(150),
+    storeEmail varchar(50)
+);
+
+create table Reservations (
+	reservationID int not null unique auto_increment primary key,
+    customerID int not null,
+    reservationTime datetime not null,
+    numberOfGuests int not null,
+    specialRequests varchar(150),
+    foreign key(customerID) references Customers(customerID)
+);
+
+create table DiningPreferences (	
+	preferenceID int not null unique auto_increment primary key,
+    customerID int not null,
+    favoriteTable varchar(50),
+    dietaryRestrictions varchar(150),
+    foreign key(customerID) references Customers(customerID)
+);
+
+DELIMITER //
+CREATE PROCEDURE findReservations(IN customer_ID INT)
+BEGIN
+    SELECT * FROM Reservations WHERE customerID = customer_ID;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE addSpecialRequest(IN reservation_ID INT, IN requests_cell VARCHAR(150))
+BEGIN
+    UPDATE Reservations SET specialRequests = requests_cell WHERE reservationID = reservation_ID;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE addReservation(
+    IN customerName_cell VARCHAR(50),
+    IN contactInfo_cell VARCHAR(150),
+    IN reservationTime_cell DATETIME,
+    IN numberOfGuests_cell INT,
+    IN specialRequests_cell VARCHAR(150)
+)
+... BEGIN
+...     DECLARE customerID_milt INT;
+...     
+...     SELECT customerID INTO customerID_milt FROM Customers WHERE customerName = customerName_cell AND contactInfo = contactInfo_cell;
+...     
+...     IF customerID_milt IS NULL THEN
+...         INSERT INTO Customers (customerName, contactInfo) VALUES (customerName_cell, contactInfo_cell);
+...         SET customerID_milt = LAST_INSERT_ID();
+...     END IF;
+... 		INSERT INTO Reservations (customerID, reservationTime, numberOfGuests, specialRequests)
+... 		VALUES (customerID_milt, reservationTime_cell, numberOfGuests_cell, specialRequests_cell);
+...     
+... END //
+... DELIMITER ;
+... 
+... INSERT INTO Customers (customerName, contactInfo) VALUES
+... ("Julynn Pelius", "jilton@cmail.com"),
+... ("John Pelius", "johnboy@cmail.com"),
+... ("Jessiana Pelius", "Jessie@cmail.com");
+... 
+... INSERT INTO Reservations (customerID, reservationTime, numberOfGuests, specialRequests) VALUES
+... (0001, "2024-05-11 14:00:00", 6, "Window seat preferred"),
+... (0002, "2024-05-12 15:00:00", 12, NULL),
+... (0003, "2024-05-13 16:00:00", 9, "Meat options requested");
+... 
+... INSERT INTO DiningPreferences (customerID, favoriteTable, dietaryRestrictions) VALUES
+... (0001, "Table 2", "None"),
+... (0002, "Table 1", "Gluten-free"),
+... (0003, "Table 3", "Vegan");
+... 
+...     def close_connection(self):
+...         self.cursor.close()
+...         self.conn.close()
